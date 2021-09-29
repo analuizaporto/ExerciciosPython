2078cde09001184de3ac6f943747866e480fd9c4
Ana Luiza Porto <72015094+analuizaporto@users.noreply.github.com>
2021-09-29 18:27:13 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considerando U$$1,00 = R$5,42 no dia 29.09.2021.
+
+real = float(input('Quantos reais você tem na carteira? R$'))
+dolar = d / 5.42
+print('Você pode comprar {:.2f} dólares'.format(do))
