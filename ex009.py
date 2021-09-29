3617768cf7c75991102173d25d31af49a1b9525f
Ana Luiza Porto <72015094+analuizaporto@users.noreply.github.com>
2021-09-29 18:46:43 -0300

    Create ex009.py

---
+++ ex009.py
@@ -0,0 +1,5 @@
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
+
+preco = float(input('Digite o preço do produto: '))
+novo = 0.95 * p
+print('O novo preço do produto é R$ {:.2f}'.format(novo))
