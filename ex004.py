a59b4832f73ecc04813a402f961c30742bc33ec3
Ana Luiza Porto <72015094+analuizaporto@users.noreply.github.com>
2021-09-29 18:13:33 -0300

    Create ex004.py

---
+++ ex004.py
@@ -0,0 +1,7 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n =int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** 1/2
+print('O número é {}, o seu dobro é {}, o seu triplo é {}, e a sua raiz quadrada é {}'.format(n, d, t, r))
