407a41f33ae423a93db8d8e5d2d19a04be873e0e
Ana Luiza Porto <72015094+analuizaporto@users.noreply.github.com>
2021-09-30 08:52:14 -0300

    Create ex012.py

---
+++ ex012.py
@@ -0,0 +1,5 @@
+#  Faça um exercício que transforma a temperatura em graus Celsius em Farheint.
+
+c = float(input('Informa a temperatura em C: '))
+f = ((9 * c) / 5) + 32
+print('A temperatura de {} C corresponde a {}F!'.format(c, f))
